c6051688faed3bd97f8bb055c755956250aa0e05
Evan Amoranto <evanamoranto@Evans-MacBook-Air.local>
2020-02-21 21:47:28 -0800

    initil upload of boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                something
+            </div>
+        )
+    }
+}
+
+export default App;
