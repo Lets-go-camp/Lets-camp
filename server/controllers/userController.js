7996eacb8b61e96bba71adb994160abb4d8061ae
Evan Amoranto <evanamoranto@yahoo.com>
2020-02-23 13:10:19 -0800

    added code for userController.getFav and userController.addFav

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const db = require('../index.js')
 
 const api_key = process.env.API_KEY;
 
-//This file is not being use right now. We may need to use this for added functionality to delete a given user or update a users favorite list.
+
 userController.login = async (req, res, next) => {
   const user = req.body.username;
   const password = req.body.password;
@@ -68,6 +68,7 @@ userController.deleteUser = (req, res, next) => {
   })
 }
 
+
 userController.createUser = (req, res, next) => {
   const user = JSON.stringify(req.body.username);
   const password = JSON.stringify(req.body.password);
@@ -90,28 +91,47 @@ userController.createUser = (req, res, next) => {
   }) 
 }
 
-userController.addFav = (req, res, next) => {
 
-}
+
+userController.addFav = (req, res, next) => {
+  const user = JSON.stringify(req.body.username);
+
+  const text = `INSERT INTO favorites (user_id, campground_id) VALUES ($1,$2)`;
+  const values = [user,campground];
+
+  db.query(text,values)
+  .then((response) => {
+    console.log('this is from userController.addFav')
+    return next();
+  })
+  .catch(err => {
+    console.log('Error: ',err);
+    return next(err)
+  });
+};
+
 
 userController.getFav = (req, res, next) => {
-  
+  const user = JSON.stringify(req.body.username);
+
+  const text = `SELECT * FROM campground WHERE campground_id in
+  (
+  select campground_id from favorites where user_id = $1
+  )`;
+
+  const value = [user];
+
+  db.query(text,value)
+  .then((response) => {
+    res.locals.user = response.rows;
+    return next();
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+    return next(err)
+  }) 
 }
 
 
-/*userController.--------- = (req,res,next) => {
-     fetch('url')
-    .then(result => result.json())
-    .then(data =>{
-        console.log('hi');
-        next();
-    })
-    .catch(err => {
-      next({
-        log: `userController.createUser: ERROR: ${err}`,
-        message: { err: 'userController.createUser: ERROR: Check server logs for details' },
-    })
-  })
-*/
 
 module.exports = userController 
