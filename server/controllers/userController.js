f84408e4a09617ff8cc768876466dc54a938dc44
Elizabeth Soto <elizabeths87@icloud.com>
2020-02-22 21:16:22 -0800

    before pulling down upstream stage

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,43 @@
 const fetch  = require('node-fetch');
 const userController = {};
+const db = require('../index.js')
 
 const api_key = process.env.API_KEY;
 
 //This file is not being use right now. We may need to use this for added functionality to delete a given user or update a users favorite list.
-userController.login = (req, res, next) => {
+userController.login = async (req, res, next) => {
+  const user = req.body.username;
+  const password = req.body.password;
 
+  const text = `SELECT * from users WHERE userName = $1`
+  const values = [user]
+  await db.query(text,values, (err, data) => {
+    if(err) {
+      console.log(err);
+        return next(err)
+    }
+    else {
+      if(data.rows[0].password !== password) {
+        console.log('password did not match')
+      }
+      else {
+        res.locals.user = data.rows[0]
+      }
+    }
+  })
+
+  const newQuery = `UPDATE users SET session = true WHERE username = $1`
+
+  db.query(newQuery,values, (err, data) => {
+    if(err){
+      console.log(err)
+      return next(err)
+    }
+    else {
+      console.log('isupdating')
+      return next();
+    }
+    })
 }
 
 userController.loginVerify = (req, res, next) => {
@@ -17,11 +49,45 @@ userController.updateUser = (req, res, next) => {
 }
 
 userController.deleteUser = (req, res, next) => {
+  const user = JSON.stringify(req.body.username);
 
+  const text = `DELETE FROM user WHERE name = $1`
+  const value = [user]
+
+  db.query(text, value, (err, data) => {
+    if(err){
+      console.log(err);
+      return next(err);
+    }
+    else {
+      if(!data){
+        return next(err);
+      }
+      return next();
+    }
+  })
 }
 
 userController.createUser = (req, res, next) => {
+  const user = JSON.stringify(req.body.username);
+  const password = JSON.stringify(req.body.password);
 
+  console.log(user, 'this is user');
+  console.log('this is req.body: ', req.body);
+
+  const text = `INSERT INTO Users (username, password, loggedin) VALUES ($1, $2, $3)`;
+  const values = [user, password, true];
+
+  db.query(text,values)
+  .then((response) => {
+    console.log('res in login: ', response);
+    res.locals.user = response.rows;
+    return next();
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+    return next(err)
+  }) 
 }
 
 userController.addFav = (req, res, next) => {
